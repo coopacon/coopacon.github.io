dc11936a020d191e26154e7415ce19c699daed42
coopacon <icooper2@uncc.edu>
2024-10-29 15:12:48 -0400

    validation fix

--- itis3135/scripts/survey.js
+++ itis3135/scripts/survey.js
@@ -1,18 +1,37 @@
 let thisPage = window.location.href;
-document.getElementById("html-validator").href = "https://validator.w3.org/check?uri=" + thisPage;
-document.getElementById("CSS-validator").href =  "https://jigsaw.w3.org/css-validator/validator?uri=" + thisPage;
-
 let numberOfFiles = 2;
 function addField(){
     numberOfFiles++;
 
-    alert("adding field");
-    //const newDiv = document.createElement("div");
+    
+    const newDiv = document.createElement("div");
+
     const newEntry = document.createElement("input");
     newEntry.type = "text";
     newEntry.id = "file"+numberOfFiles;
-
+    
     newDiv.appendChild(newEntry);
-
-    document.getElementById("multipleFields").appendChild
+    
+    document.getElementById("multipleFields").appendChild(newDiv);
+    
 }
+function addField(){
+    numberOfFiles++;
+
+    
+    const newDiv = document.createElement("div");
+
+    const newEntry = document.createElement("input");
+    newEntry.type = "text";
+    newEntry.id = "file"+numberOfFiles;
+    
+    newDiv.appendChild(newEntry);
+    
+    document.getElementById("multipleFields").appendChild(newDiv);
+    
+}
+alert("validation")
+document.getElementById("html-validator").href = "https://validator.w3.org/check?uri=" + thisPage;
+alert("validation 2")
+document.getElementById("CSS-validator").href =  "https://jigsaw.w3.org/css-validator/validator?uri=" + thisPage;
+alert("done with validation")
