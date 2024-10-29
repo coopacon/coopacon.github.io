5106093b8d81b62c389517f5a2829f8fb0251213
coopacon <icooper2@uncc.edu>
2024-10-29 14:45:52 -0400

    adding BYO Intro (#6)

---
+++ itis3135/scripts/survey.js
@@ -0,0 +1,18 @@
+let thisPage = window.location.href;
+document.getElementById("html-validator").href = "https://validator.w3.org/check?uri=" + thisPage;
+document.getElementById("CSS-validator").href =  "https://jigsaw.w3.org/css-validator/validator?uri=" + thisPage;
+
+let numberOfFiles = 2;
+function addField(){
+    numberOfFiles++;
+
+    alert(adding field)
+    //const newDiv = document.createElement("div");
+    const newEntry = document.createElement("input");
+    newEntry.type = "text";
+    newEntry.id = "file"+numberOfFiles;
+
+    newDiv.appendChild(newEntry);
+
+    document.getElementById("multipleFields").appendChild
+}
