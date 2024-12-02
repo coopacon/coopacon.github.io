86be981aa83f1b9f51150a8ac3c9679f84e79b0a
coopacon <icooper2@uncc.edu>
2024-12-02 11:14:40 -0500

    Project update

--- client-project/scripts/default.js
+++ client-project/scripts/default.js
@@ -47,30 +47,4 @@ function next (){
 }
 
 //Event listener for the next button
-document.getElementById("next-page").addEventListener("click", next);
-
-//Get every location where data needs to be imported
-const placeToInclude = document.querySelectorAll("[data-include]");
-//Loop for every element
-placeToInclude.forEach( async (element) => {
-    //Location path to element
-    const dataLocation = element.getAttribute("data-include");
-    //If location is found
-    if (dataLocation){
-        try {
-            //Establish the data to be used
-            const dataToInclude = await fetch(dataLocation);
-            if (dataToInclude.ok){
-                //If data is valid, then replace the elements content w/ the respective text
-                const content = await dataToInclude.text();
-                element.innerHTML = content;
-            } else {
-                //Error Handling
-                console.error(error.content);
-            }
-        //Error Handling
-        } catch (error){
-            console.error(error.content);
-        }
-    }
-});
+document.getElementById("next-page").addEventLis
